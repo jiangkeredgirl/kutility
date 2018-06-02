2672740e378219dacac26cc1d0644af4eb2eacc8
jiangke <jiangkeredgirl@hotmail.com>
2018-06-02 12:17:16 +0800

    add qtstandard.h

---
+++ qtstandard.h
@@ -0,0 +1,35 @@
+﻿#ifndef QTSTANDARD_H
+#define QTSTANDARD_H
+
+
+#include <QPainter>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QEvent>
+#include <QMouseEvent>
+#include <QListView>
+#include <QListWidget>
+#include <QDebug>
+#include <QMenu>
+#include <QFileDialog>
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QSignalMapper>
+#include <QRadioButton>
+#include <QDesktopWidget>
+#include <QApplication>
+#include <QMessageBox>
+#include <QCheckBox>
+#include <QTextEdit>
+#include <QRect>
+#include <QAbstractScrollArea>
+#include <QScrollBar>
+#include <QProgressBar>
+#include <QMenuBar>
+#include <QButtonGroup>
+
+
+#endif // QTSTANDARD_H
